f25418ff7f21013dff6fe7ec81a464cb920d6d63
John Ament <john.d.ament@gmail.com>
2017-12-05 16:21:31 -0500

    Adding release script.

---
+++ buildScripts/release.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+    tools {
+        maven 'apache-maven-latest'
+        jdk 'jdk1.8.0-latest'
+    }
+    parameters {
+        string(description: 'The next snapshot version', name: 'snapshotVersion')
+        string(description: 'The release version', name: 'releaseVersion')
+        string(description: 'The SCM tag to apply', name: 'tag')
+        choice(choices: 'Draft\nFinal', description: 'Revision Type', name: 'revremark')
+        choice(choices: 'microprofile-rest-client\nmicroprofile-fault-tolerance\nmicroprofile-metrics\nmicroprofile-open-api', description: 'Module', name: 'module')
+        string(description: 'Branch to use', name: 'branch', defaultValue: 'master')
+    }
+
+    stages {
+        stage("Checkout") {
+            steps {
+                git url: "git@github.com:eclipse/${params.module}.git", branch: params.branch
+            }
+        }
+        stage("Execute Release") {
+            steps {
+                sh "mvn -s /opt/public/hipp/homes/genie.microprofile/.m2/settings-deploy-ossrh.xml release:prepare release:perform -B -Dtag=${params.tag} -DdevelopmentVersion=${params.snapshotVersion} -DreleaseVersion=${params.releaseVersion} -Drevremark=${params.revremark}"
+            }
+        }
+    }
+    post {
+        always {
+            archive 'spec/target/generated-docs/*'
+            deleteDir()
+        }
+    }
+}
+
