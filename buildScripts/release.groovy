404fa6be429e8954b1626e3521dd2d7fec7355f0
Roberto Cortez <radcortez@yahoo.com>
2020-11-19 15:21:15 +0000

    Copy all TCK modules files.

--- buildScripts/release.groovy
+++ buildScripts/release.groovy
@@ -52,7 +52,7 @@ pipeline {
                             sh "scp -r api/target/apidocs/* genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/apidocs"
                         }
                         if (fileExists('tck')) {
-                            sh "scp tck/target/*.jar genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/"
+                            sh "find tck -name \"*.jar\" | xargs -I{} scp {} genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/"
                         }
                     }
                 }
