d92aae1712dc4474d5095510b6504933a9e9f3c9
xstefank <xstefank122@gmail.com>
2021-02-08 09:57:10 +0100

    Move the release execution to the params.module directory

--- buildScripts/release.groovy
+++ buildScripts/release.groovy
@@ -22,51 +22,57 @@ pipeline {
     stages {
         stage("Checkout") {
             steps {
-                git credentialsId: 'github-bot-ssh', url: "git@github.com:eclipse/${params.module}.git", branch: params.branch
+                dir("${params.module}") {
+                    git credentialsId: 'github-bot-ssh', url: "git@github.com:eclipse/${params.module}.git", branch: params.branch
+                }
             }
         }
         stage("Execute Release") {
             steps {
-                withCredentials([file(credentialsId: 'secret-subkeys.asc', variable: 'KEYRING')]) {
-                    sh 'gpg --batch --import "${KEYRING}"'
-                    sh 'for fpr in $(gpg --list-keys --with-colons  | awk -F: \'/fpr:/ {print $10}\' | sort -u); do echo -e "5\ny\n" |  gpg --batch --command-fd 0 --expert --edit-key ${fpr} trust; done'
-                }
-                sshagent(['github-bot-ssh']) {
-                    sh '''
-                        git config --global user.email "microprofile-bot@eclipse.org"
-                        git config --global user.name "Eclipse MicroProfile bot"
-                    '''
+                dir("${params.module}") {
+                    withCredentials([file(credentialsId: 'secret-subkeys.asc', variable: 'KEYRING')]) {
+                        sh 'gpg --batch --import "${KEYRING}"'
+                        sh 'for fpr in $(gpg --list-keys --with-colons  | awk -F: \'/fpr:/ {print $10}\' | sort -u); do echo -e "5\ny\n" |  gpg --batch --command-fd 0 --expert --edit-key ${fpr} trust; done'
+                    }
+                    sshagent(['github-bot-ssh']) {
+                        sh '''
+                            git config --global user.email "microprofile-bot@eclipse.org"
+                            git config --global user.name "Eclipse MicroProfile bot"
+                        '''
 
-                    script {
-                        def settings = '-s /home/jenkins/.m2/settings.xml'
+                        script {
+                            def settings = '-s /home/jenkins/.m2/settings.xml'
 
-                        if (params.stagingList != '') {
-                            sh "wget https://github.com/eclipse/microprofile-parent/raw/master/buildScripts/staging-augmenter-ubi8"
-                            sh "chmod +x ./staging-augmenter-ubi8"
-                            sh "./staging-augmenter-ubi8 -r ${params.stagingList} -o ../output-settings.xml /home/jenkins/.m2/settings.xml"
+                            if (params.stagingList != '') {
+                                sh "wget https://github.com/eclipse/microprofile-parent/raw/master/buildScripts/staging-augmenter-ubi8"
+                                sh "chmod +x ./staging-augmenter-ubi8"
+                                sh "./staging-augmenter-ubi8 -r ${params.stagingList} -o ../output-settings.xml /home/jenkins/.m2/settings.xml"
 
-                            settings = '-s ../output-settings.xml -Pmp-staging'
+                                settings = '-s ../output-settings.xml -Pmp-staging'
+                            }
+
+                            sh "mvn ${settings} release:prepare release:perform -B -Dtag=${params.tag} -DdevelopmentVersion=${params.snapshotVersion} -DreleaseVersion=${params.releaseVersion} -Drevremark=${params.revremark} -Drelease.revision=${params.revremark} -DstagingProgressTimeoutMinutes=20"
                         }
-
-                        sh "mvn ${settings} release:prepare release:perform -B -Dtag=${params.tag} -DdevelopmentVersion=${params.snapshotVersion} -DreleaseVersion=${params.releaseVersion} -Drevremark=${params.revremark} -Drelease.revision=${params.revremark} -DstagingProgressTimeoutMinutes=20"
                     }
                 }
             }
         }
         stage("Copy Specs") {
             steps {
-                sshagent ( ['projects-storage.eclipse.org-bot-ssh']) {
-                    sh "ssh genie.microprofile@projects-storage.eclipse.org mkdir -p /home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}"
-                    sh "scp -r spec/target/generated-docs/* genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}"
-                    script {
-                        if (fileExists('api')) {
-                            sh "scp api/target/*.jar genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/"
+                dir("${params.module}") {
+                    sshagent(['projects-storage.eclipse.org-bot-ssh']) {
+                        sh "ssh genie.microprofile@projects-storage.eclipse.org mkdir -p /home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}"
+                        sh "scp -r spec/target/generated-docs/* genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}"
+                        script {
+                            if (fileExists('api')) {
+                                sh "scp api/target/*.jar genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/"
 
-                            sh "ssh genie.microprofile@projects-storage.eclipse.org mkdir -p /home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/apidocs"
-                            sh "scp -r api/target/apidocs/* genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/apidocs"
-                        }
-                        if (fileExists('tck')) {
-                            sh "find tck -name \"*.jar\" | xargs -I{} scp {} genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/"
+                                sh "ssh genie.microprofile@projects-storage.eclipse.org mkdir -p /home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/apidocs"
+                                sh "scp -r api/target/apidocs/* genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/apidocs"
+                            }
+                            if (fileExists('tck')) {
+                                sh "find tck -name \"*.jar\" | xargs -I{} scp {} genie.microprofile@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/microprofile/staging/${params.module}-${params.releaseVersion}/"
+                            }
                         }
                     }
                 }
@@ -75,9 +81,11 @@ pipeline {
     }
     post {
         always {
-            archive 'spec/target/generated-docs/*'
+            dir("${params.module}") {
+                archive 'spec/target/generated-docs/*'
+                sh "rm -f ../output-settings.xml"
+            }
             deleteDir()
-            sh "rm -f ../output-settings.xml"
         }
     }
 }
